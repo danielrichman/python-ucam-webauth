f60458e25260fec3840e49bdd69546ecb8ec7fa6
Daniel Richman <main@danielrichman.co.uk>
2013-07-27 16:29:20 +0000

    Version bump (0.2.1)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = "python-raven",
-    version = "0.2",
+    version = "0.2.1",
     packages = ["raven"],
     py_modules = ["ucam_webauth"],
     package_data = {"raven": ["keys/pubkey*.crt"]},
